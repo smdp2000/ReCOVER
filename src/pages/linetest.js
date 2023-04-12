d919322446dc6351a94b9ef9f360f053e71135cd
Ryan Lee <rzlee@usc.edu>
2023-04-12 15:12:59 -0700

    somewhat working

---
+++ src/pages/linetest.js
@@ -0,0 +1,169 @@
+import { useState, useEffect } from 'react'
+import 'chart.js/auto'
+import { Chart } from 'react-chartjs-2'
+import { readString } from 'react-papaparse'
+import axios from 'axios'
+import { FormControl, InputLabel, Select, MenuItem } from '@mui/material'
+
+const fileList = {
+    cases: 'https://raw.githubusercontent.com/scc-usc/ReCOVER-COVID-19/master/results/forecasts/us_data.csv',
+    deaths: 'https://raw.githubusercontent.com/scc-usc/ReCOVER-COVID-19/master/results/forecasts/us_deaths.csv',
+    pred_cases:
+        'https://raw.githubusercontent.com/scc-usc/ReCOVER-COVID-19/master/results/forecasts/us_forecasts_current_0.csv',
+    pred_deaths:
+        'https://raw.githubusercontent.com/scc-usc/ReCOVER-COVID-19/master/results/forecasts/us_deaths_current_0.csv',
+}
+
+function linetest() {
+    const [fileState, setFileState] = useState({
+        name: 'cases',
+        url: fileList['cases'],
+    })
+
+    const [chartState, setChartState] = useState({
+        category: '',
+        labels: [],
+        dates: [],
+        cases: [],
+        all_datasets: [],
+        current_dataset: {
+            labels: [],
+            datasets: [
+                {
+                    label: 'dummy data',
+                    data: [],
+                    fill: false,
+                    borderColor: 'rgb(75, 192, 192)',
+                    tension: 0.1,
+                },
+            ],
+        },
+    })
+
+    const fetchData = async () => {
+        await axios.get(fileState.url).then((response) => {
+            readString(response.data, {
+                worker: true,
+                complete: (results) => {
+                    console.log(results.data)
+
+                    let labels = []
+                    let cases = []
+                    let datasets = []
+
+                    for (let i = 1; i < results.data.length; ++i) {
+                        if (results.data[i].length > 2) {
+                            labels[i - 1] = results.data[i][1]
+                            cases.push(results.data[i].slice(2))
+                        }
+                    }
+
+                    for (let i = 0; i < labels.length; ++i) {
+                        datasets.push({
+                            label: labels[i],
+                            data: cases[i],
+                            fill: false,
+                            borderColor: 'rgb(75, 192, 192)',
+                            tension: 0.1,
+                        })
+                    }
+
+                    setChartState({
+                        ...chartState,
+                        category: results.data[0][1],
+                        labels: labels,
+                        dates: results.data[0].slice(2),
+                        cases: cases,
+                        all_datasets: datasets,
+                        current_dataset: {
+                            labels: results.data[0].slice(2),
+                            datasets: [
+                                {
+                                    label: labels[0],
+                                    data: cases[0],
+                                    fill: false,
+                                    borderColor: 'rgb(75, 192, 192)',
+                                    tension: 0.1,
+                                },
+                            ],
+                        },
+                    })
+                },
+            })
+        })
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [fileState])
+
+    const handleFileChange = (e) => {
+        const newFile = e.target.value
+        setFileState({
+            name: newFile,
+            url: fileList[newFile],
+        })
+    }
+
+    const handleLabelChange = (e) => {
+        const newLabel = e.target.value
+        let i
+
+        for (i = 0; i < chartState.labels.length; ++i)
+            if (chartState.labels[i] === newLabel) break
+
+        setChartState({
+            ...chartState,
+            current_dataset: {
+                labels: chartState.dates,
+                datasets: [
+                    {
+                        label: chartState.labels[i],
+                        data: chartState.cases[i],
+                        fill: false,
+                        borderColor: 'rgb(75, 192, 192)',
+                        tension: 0.1,
+                    },
+                ],
+            },
+        })
+    }
+
+    return (
+        <div>
+            <FormControl>
+                <InputLabel id="input-label">File</InputLabel>
+                <Select
+                    labelId="file-id"
+                    id="select"
+                    value={fileState.name}
+                    label="category"
+                    onChange={handleFileChange}
+                >
+                    {Object.keys(fileList).map((fileName) => {
+                        return <MenuItem value={fileName}>{fileName}</MenuItem>
+                    })}
+                </Select>
+            </FormControl>
+            <FormControl>
+                <InputLabel id="input-label">{chartState.category}</InputLabel>
+                <Select
+                    labelId="label-id"
+                    id="select"
+                    value={chartState.current_dataset.datasets[0].label}
+                    label="category"
+                    onChange={handleLabelChange}
+                >
+                    {chartState.labels.map((label) => {
+                        return <MenuItem value={label}>{label}</MenuItem>
+                    })}
+                </Select>
+            </FormControl>
+            <div className="chart">
+                <Chart type="line" data={chartState.current_dataset} />
+            </div>
+        </div>
+    )
+}
+
+export default linetest
